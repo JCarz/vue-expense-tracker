78fbdf104454882660a7811b8e5845daf8d19e13
Jaylen Carroll <jaylencarroll87@gmail.com>
2024-01-25 17:03:32 -0500

    Added expense Tracker with Transaction assests, history,array with key applying dynamic class based transaction,CSS Styling

---
+++ src/components/Balance.vue
@@ -0,0 +1,4 @@
+<template>
+  <h4>Add Balance</h4>
+  <h1 id="balance">$0.00</h1>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>History</h3>
+
+  <ul id="list" class="list">
+    <!-- Using Vue.js v-for directive to loop through transactions array -->
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      <!-- Setting a unique key for each transaction and Applying dynamic class based on transaction amount 'minus' : 'plus' -->
+      >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Wallpaper", amount: 1300.99 },
+        { id: 3, text: "teesst", amount: -14.99 },
+      ],
+    }
+  },
+}
+</script>
